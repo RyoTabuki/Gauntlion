00ff407d99da585040b1ff1897e98f630b542306
RyoTabuki <127320006+RyoTabuki@users.noreply.github.com>
2023-03-10 00:19:20 +0200

    Add files via upload

---
+++ Gauntlion/FilesInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauntlion
+{
+    public class FilesInfo
+    {
+        public string ReadText(string pathname)
+        {
+            String const_name = String.Empty;
+            string m = File.ReadAllText(pathname);
+            const_name = m;
+            return const_name;
+        }
+    }
+}
---
+++ Gauntlion/ProcessInformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauntlion
+{
+    public class ProcessInformation
+    {
+        public byte[] GetBytesFiles(string filepath)
+        {
+            byte[] read_file_data = File.ReadAllBytes(filepath);
+            return read_file_data;
+        }
+        public int GetProcessID()
+        {
+            Process procession = Process.GetCurrentProcess();
+            return procession.Id;
+        }
+        public int Gauntlion_GetProcessIDCurrent(string processname)
+        {
+            int process_id = 0;
+            Process[] x = Process.GetProcessesByName(processname);
+            foreach (Process process in x)
+            {
+                process_id = process.Id;
+            }
+            return process_id;
+        }
+    }
+}
